a5344267cefdbf6acd746126a7540c46e8aadca8
chaechae <juold0568@naver.com>
2025-06-03 11:42:09 +0900

    refactor: util kotlin 변환

---
+++ src/main/java/com/est/curdsample/util/TimeFormatter.kt
@@ -0,0 +1,21 @@
+package com.est.curdsample.util
+
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
+object TimeFormatter {
+    private val pattern: DateTimeFormatter = DateTimeFormatter.ofPattern("MM/dd/yyyy")
+
+    fun convertToLocalDate(localDateStr: String?): LocalDate {
+        return LocalDate.parse(localDateStr, pattern)
+    }
+
+    fun convertToString(localDate: LocalDate): String {
+        return localDate.format(pattern)
+    }
+
+    fun convertToString(localDateTime: LocalDateTime): String {
+        return localDateTime.format(pattern)
+    }
+}
