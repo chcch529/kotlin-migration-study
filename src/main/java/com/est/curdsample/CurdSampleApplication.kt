c76c406b9519ad39cdbf05c6a26f3bf044020d5d
chaechae <juold0568@naver.com>
2025-06-04 10:17:31 +0900

    refactor: main kotlin 변환

---
+++ src/main/java/com/est/curdsample/CurdSampleApplication.kt
@@ -0,0 +1,11 @@
+package com.est.curdsample
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CurdSampleApplication
+
+fun main(args: Array<String>) {
+    runApplication<CurdSampleApplication>(*args)
+}
